fe62569eec92dfec0fc6e4583eb7b1499c213de6
fedecrux <federicocruciani@gmail.com>
2019-11-08 14:59:24 +0000

    Utility class

---
+++ IMU/utils.py
@@ -0,0 +1,19 @@
+'''
+File: Utils.py
+Author: Federico Cruciani
+Date: 03/10/2019
+Version: 1.0
+Description: 
+	Generic utility functions.	 
+'''
+
+
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+	percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+	filledLength = int(length * iteration // total)
+	bar = fill * filledLength + '-' * (length - filledLength)
+	print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+	# Print New Line on Complete
+	if iteration == total:
+		print()
